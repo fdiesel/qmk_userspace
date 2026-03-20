1eaba3809cecdf1cf1bf8bb23d57ffc91f80b9c6
fdiesel <72613036+fdiesel@users.noreply.github.com>
2026-03-20 09:55:44 +0100

    fix: overwrite default rgb eeprom

--- keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
@@ -63,6 +63,11 @@ enum custom_keycodes {
     CU_GRV,
 };
 
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_BREATHING);
+    rgb_matrix_sethsv_noeeprom(145, 210, 160);
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CU_AE:
