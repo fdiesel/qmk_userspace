92a69cbab3e5db9e181433417cac44c359d0a39c
fdiesel <72613036+fdiesel@users.noreply.github.com>
2026-03-30 01:43:35 +0200

    feat: make shift keys general

--- keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
@@ -420,12 +420,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
  const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_split_3x6_5_hlc(
-       'L', 'L', 'L', 'L', 'L', 'L',                                    'R', 'R', 'R', 'R', 'R', 'R',
-       'L', 'L', 'L', 'L', 'L', 'L',                                    'R', 'R', 'R', 'R', 'R', 'R',
-       'L', 'L', 'L', 'L', 'L', 'L', '*', '*', '*', '*', 'R', 'R', 'R', 'R', 'R', 'R',
+       'L', 'L', 'L', 'L', 'L', 'L',                     'R', 'R', 'R', 'R', 'R', 'R',
+       'L', 'L', 'L', 'L', 'L', 'L',                     'R', 'R', 'R', 'R', 'R', 'R',
+       '*', 'L', 'L', 'L', 'L', 'L', '*', '*', '*', '*', 'R', 'R', 'R', 'R', 'R', '*',
                       '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
 
-       'L', 'L', 'L', 'L', 'L',                                              'R', 'R', 'R', 'R', 'R'
+       'L', 'L', 'L', 'L', 'L',                               'R', 'R', 'R', 'R', 'R'
 );
 
 
