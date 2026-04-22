15aab26c253bf665bcc28ceff061a2d5f2b611dc
fdiesel <72613036+fdiesel@users.noreply.github.com>
2026-04-22 11:13:03 +0200

    feat: right pinky ent (for mouse)

--- keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/fdiesel_hlc/keymap.c
@@ -235,7 +235,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_5_hlc(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y ,   KC_U ,  KC_I ,   KC_O ,  KC_P ,  CU_UE ,
      CTL_ESC , HR_A ,  HR_S   ,  HR_D  ,   HR_F ,   KC_G ,                                         KC_H ,   HR_J ,  HR_K ,   HR_L , CU_OE ,  CU_AE ,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , LGUI(LALT(KC_K)),LGUI(LSFT(KC_S)),     CU_IDLE, FKEYS  ,  KC_N ,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,  KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , LGUI(LALT(KC_K)),LGUI(LSFT(KC_S)),     CU_IDLE, FKEYS  ,  KC_N ,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,  KC_ENT,
                                 ADJUST , KC_LGUI, ALT_ENT, CMD,FUN    ,     NAV    , SYM    ,NUMBERS, KC_RGUI, KC_APP,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
